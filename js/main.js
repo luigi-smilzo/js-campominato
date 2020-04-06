8562e14329b055a7bf7340768164c12730963e09
Luigi <luigismilzo@live.com>
2020-04-06 15:51:11 +0200

    Bombs

---
+++ js/main.js
@@ -0,0 +1,41 @@
+/**
+ * Il computer deve generare 16 numeri casuali tra 1 e 100 (numeri vietati).
+In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100.
+L’utente non può inserire più volte lo stesso numero.
+Se il numero è presente nella lista dei numeri generati (numeri vietati), la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+La partita termina quando il giocatore inserisce un numero “vietato”o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+BONUS:
+All’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+con difficoltà 0 => tra 1 e 100
+con difficoltà 1 =>  tra 1 e 80
+con difficoltà 2=> tra 1 e 50
+ */
+
+var bombs = [];
+randomCpu(bombs, 1, 100);
+
+
+
+
+
+
+
+
+
+
+
+
+/* Functions */
+
+function randomCpu (array, min, max) {
+    
+    while (array.length < 16) {
+        var number = Math.floor( Math.random() * (max - min) + min );
+
+        if ( !array.includes(number) ) {
+            array.push(number);
+        }
+    }
+}
